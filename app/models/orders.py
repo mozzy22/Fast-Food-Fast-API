4365c58b262937763d246e50d7de3fe9d9fd070f
mozzy11 <mozzymutesa@gmail.com>
2018-09-20 07:18:58 +0300

    linting [#160550567]

--- app/models/orders.py
+++ app/models/orders.py
@@ -1,3 +1,5 @@
+"A module for the orders model"
+
 import uuid
 import datetime
 __author__ = "Mutesasira Moses"
@@ -14,7 +16,7 @@ class Orders:
     def __init__(self):
         "innitialising all class members of the order - model"
         self.orders_list = []
-        self.order  = {}
+        self.order = {}
         self.order_id = 0
         self.order_uuid = ""
         self.order_food_id = ""
@@ -39,10 +41,10 @@ class Orders:
     def place_order(self, order_food_id, order_quantity, order_client):
         "A method to place a new order"
         self.order_id = len(self.orders_list) + 1
-        self.order_uuid =  str(uuid.uuid1())
+        self.order_uuid = str(uuid.uuid1())
         self.order_food_id = order_food_id
         self.order_quantity = order_quantity
-        self.order_created_at =  datetime.date.today()
+        self.order_created_at = datetime.date.today()
         self.order_status = Orders.STATUS1
         self.order_client = order_client
 
@@ -61,36 +63,39 @@ class Orders:
     def validate_order_obj(self, order_Obj):
         "A method to validate a user object"
         if ("order_food_id" in order_Obj and "order_quantity" in order_Obj
-          and "order_client" in order_Obj):
+              and "order_client" in order_Obj):
             return True
         else:
             return False
 
 
     def get_all_foods(self):
+        "A method to return all availabe food list"
         return self.food_list
 
 
     def add_food(self, food_name, food_price):
-         self.food_id = len(self.food_list) + 1
-         self.food_name = food_name
-         self.food_price = food_price
-         self.food = {
-             "food_id": self.food_id,
-             "food_name": self.food_name,
-             "food_price": self.food_price
+        "A  method to add foods items to the menu"
+        self.food_id = len(self.food_list) + 1
+        self.food_name = food_name
+        self.food_price = food_price
+        self.food = {
+            "food_id": self.food_id,
+            "food_name": self.food_name,
+            "food_price": self.food_price
          }
-         self.food_list.append(self.food)
+        self.food_list.append(self.food)
 
     def validate_food_obj(self, food_Obj):
         "A method to validate a food object"
-        if ("food_name" in food_Obj and "food_price" in food_Obj) :
+        if ("food_name" in food_Obj and "food_price" in food_Obj):
             return True
         else:
             return False
 
 
-    def check_existing_order(self,order_food_id, order_client):
+    def check_existing_order(self, order_food_id, order_client):
+        "a method to check whethr a given order already exists"
         exist = True
         order_created_at = datetime.date.today()
         for order in self.orders_list:
@@ -104,16 +109,29 @@ class Orders:
         return exist
 
 
-    def check_existing_food_id(self, order_food_id):
+    def check_existing_food_by_id(self, order_food_id):
+        "A method to check whether a given food item already exists on the menu"
         exist = True
         for food in self.food_list:
-            if food["food_id"] == order_food_id :
+            if food["food_id"] == order_food_id:
                 exist = True
                 break
-            else :
+            else:
                 exist = False
                 #print(food)
         #print(exist)
+        return exist
+
+    def check_existing_food_by_name(self, food_name):
+        "A method to check whether a given food item already exists on the menuby name"
+        exist = True
+        for food in self.food_list:
+            if food["food_name"] == food_name:
+                exist = True
+                break
+            else:
+                exist = False
+
         return exist
 
 # if __name__ == "__main__":
--- tests/test_place_order.py
+++ tests/test_place_order.py
@@ -22,30 +22,35 @@ class Test_Case(unittest.TestCase):
             "order_client": "mutesasira"
 
         }
+
+        #valid order to be pasted
         self.order = {
              "order_food_id" : 1,
             "order_quantity" : "1",
             "order_client" :"mutesasira"
         }
 
+       #order with non existent food id
         self.order_invalid_food_id = {
             "order_food_id": 9,
             "order_quantity": "1",
             "order_client": "mutesasira"
         }
 
+       # inalid order format
         self.invalid_order = {
             "order_quantity": "1",
             "order_client": "mutesasira"
         }
 
 
-
+        # valid food objet to be posted
         self.food = {
             "food_name": "fish",
             "food_price": "25$"
         }
 
+      #invalid food object to be pasted
         self.food_invalid = {
             "inavlid key": "fish",
             "food_price": "25$"
@@ -57,76 +62,103 @@ class Test_Case(unittest.TestCase):
 
 #testing the model methods
     def test_model_creation(self):
+        "A method to test model object creation"
         self.assertIsInstance(self.order_obj, Orders)
 
     def test_empty_model_oder_list(self):
+        "A method to assert no dummy data in mode oredr list"
         self.assertEquals(len(self.order_obj.get_all_orders()), 0)
 
     def test_place_order(self):
+        " a method to assert model place order method"
         self.order_obj.place_order(1,3,"moses")
         self.assertEquals(len(self.order_obj.get_all_orders()), 1)
 
     def test_validate_order(self):
+         " method to aasert validation of oder order dictionaryobject"
          self.assertFalse(self.order_obj.validate_order_obj(self.invalid_order))
 
     def test_empty_food_list(self):
+        "method to assert empty model food list"
         self.assertEqual(len(self.order_obj.get_all_foods()),0)
 
     def test_add_food(self):
+         "ethod to assert food added"
          self.order_obj.add_food("pizza", "20$")
          self.assertEqual(len(self.order_obj.get_all_foods()),1)
 
     def test_validate_food(self):
+         "method to assert valid food dictionary object"
          self.assertTrue(self.order_obj.validate_food_obj(self.food))
 
     def test_validate_food2(self):
+        "method to asert valid invalid food object"
         self.assertFalse(self.order_obj.validate_food_obj(self.food_invalid))
 
 
     def test_check_exist_order(self):
+         "method to assert order exists"
          self.order_obj.place_order(1, 3, "moses")
          self.assertTrue(self.order_obj.check_existing_order(1,"moses"))
 
     def test_check_exist_order2(self):
+        "method to assert order doesnt exists"
         self.order_obj.place_order(1, 3, "moses")
         self.assertFalse(self.order_obj.check_existing_order(2, "moses"))
 
     def test_exist_food_id(self):
+        "method to assert food id exists"
         self.order_obj.add_food("pizza", "20$")
-        self.assertTrue(self.order_obj.check_existing_food_id(1))
+        self.assertTrue(self.order_obj.check_existing_food_by_id(1))
 
     def test_exist_food_id2(self):
+        "method to assert food id doesnt exists"
         self.order_obj.add_food("pizza", "20$")
         self.order_obj.add_food("chicken", "20$")
-        self.assertFalse(self.order_obj.check_existing_food_id(3))
+        self.assertFalse(self.order_obj.check_existing_food_by_id(3))
 
+    def test_exist_food_name(self):
+        "method to assert food exists byname"
+        self.order_obj.add_food("pizza", "20$")
+        self.assertTrue(self.order_obj.check_existing_food_by_name("pizza"))
+
+    def test_exist_food_name2(self):
+        "method to assert food  doesnt exist by name"
+        self.order_obj.add_food("pizza", "20$")
+        self.assertFalse(self.order_obj.check_existing_food_by_name("none"))
 
 #Testing the routes
     def test_empty_list(self):
+        "method to assert empty order objct"
         self.assertEqual(len(order_obj.get_all_orders()),0)
 
     def test_post_valid_order(self):
+        "testing valid order is added to order list"
         resp = self.app.post(self.hostname + "orders", data=json.dumps(self.order), content_type='application/json')
         self.assertEqual(len(order_obj.get_all_orders()), 1)
         print(str(resp.data))
 
     def test_post_valid_order2(self):
+        "testing valid order is added to order list"
         order_obj.orders_list.append(self.complete_order)
         resp = self.app.post(self.hostname + "orders", data=json.dumps(self.order), content_type='application/json')
         self.assertEqual(len(order_obj.get_all_orders()), 2)
 
-    def test_post_invalid_order2(self):
+    def test_post_invalid_order(self):
+        "testing object list if invalid order is posted"
         order_obj.orders_list.append(self.complete_order)
         resp = self.app.post(self.hostname + "orders", data=json.dumps(self.invalid_order), content_type='application/json')
         self.assertEqual(len(order_obj.get_all_orders()), 1)
 
-    def test_post_invalid_order3(self):
+    def test_post_invalid_order1(self):
+        "testing valid order is posted wen food list is empty"
         order_obj.orders_list.append(self.complete_order)
         order_obj.food_list.clear()
         resp = self.app.post(self.hostname + "orders", data=json.dumps(self.order), content_type='application/json')
         self.assertEqual(len(order_obj.get_all_orders()), 1)
 
     def test_post_invalid_order4(self):
+        "testing status code if valid order posted wen food list is empty"
         order_obj.orders_list.append(self.complete_order)
         order_obj.food_list.clear()
         resp = self.app.post(self.hostname + "orders", data=json.dumps(self.order), content_type='application/json')
@@ -134,24 +166,29 @@ class Test_Case(unittest.TestCase):
 
 
     def test_post_valid_order_status(self):
+        "testing status code if valid order is posted"
         resp = self.app.post(self.hostname + "orders", data=json.dumps(self.order), content_type='application/json', )
         self.assertEqual(resp.status_code, 201)
 
     def test_post_invalid_order(self):
+        "testing status code if invalid order is posted"
         resp = self.app.post(self.hostname + "orders", data=json.dumps(self.invalid_order), content_type='application/json', )
         self.assertEqual(resp.status_code, 406)
 
     def test_post_empty_order(self):
+        "testing status code if empty content is posted "
         resp = self.app.post(self.hostname + "orders" )
         self.assertEqual(resp.status_code, 406)
 
     def test_post_invalid_food_id(self):
+        "testing status code if order with non existent food id is posted"
         resp = self.app.post(self.hostname + "orders", data=json.dumps(self.order_invalid_food_id)
                              ,content_type='application/json' )
         self.assertEqual(resp.status_code, 404)
 
 
     def tearDown(self):
+        #clearing the object lists after test runs
          order_obj.orders_list.clear()
          self.order_obj.orders_list.clear()
          order_obj.food_list.clear()
