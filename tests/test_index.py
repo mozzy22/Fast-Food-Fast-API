2262a998c5e1fea3745f20190afec62992ebd51e
mozzy11 <mozzymutesa@gmail.com>
2018-10-03 17:21:13 +0300

    create documentation [#160949747]

--- tests/test_index.py
+++ tests/test_index.py
@@ -8,7 +8,7 @@ class TestGetIndex(BaseTestCase):
     "The test Class for the add_food feature"
 
     def test_get_index_page(self):
-        resp = self.app.get("http://localhost:5000/")
+        resp = self.app.get("http://localhost:5000/index")
         self.assertEqual(resp.status_code, 200)
 
 if __name__ == "__main__":
